076fd2a2609d20e8f8bd6d4b36fb551a0fa3645e
Bryce Eadie <bryce.eadie@gmail.com>
2018-09-29 07:30:50 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,11 @@
-def my_select(collection)
- # your code here!
+def my_select(array)
+ i = 0 
+ new_array = []
+ while i < array.length 
+  check = yield array[i]
+    if check == true 
+      new_array << check
+    end
+  end
+  new_array
 end
